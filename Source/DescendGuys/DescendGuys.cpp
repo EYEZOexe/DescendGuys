c30bc7207e832a1d3ff035ac611c0b7167c77457
EYEZO.exe <119551914+EYEZOexe@users.noreply.github.com>
2024-08-10 00:04:52 +0300

    Init

---
+++ Source/DescendGuys/DescendGuys.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DescendGuys.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DescendGuys, "DescendGuys" );
+ 
